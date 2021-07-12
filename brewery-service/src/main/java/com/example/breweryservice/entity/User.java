3cc3e4edf061e49afec62e33b7a7b469f76b7c83
adam <everett.ingram22@gmail.com>
2021-07-12 15:36:13 -0400

    User entity added to brewery-service

--- brewery-service/src/main/java/com/example/breweryservice/entity/User.java
+++ brewery-service/src/main/java/com/example/breweryservice/entity/User.java
@@ -1,4 +1,45 @@
 package com.example.breweryservice.entity;
 
+import java.math.BigInteger;
+
 public class User {
+    private BigInteger userid;
+    private String username;
+    private String email;
+
+    public User() {
+    }
+
+    public BigInteger getUserid() {
+        return userid;
+    }
+
+    public void setUserid(BigInteger userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userid=" + userid +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
